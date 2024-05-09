ae84e1c8f2d71c92a96d6a1ebdf0522217406d3a
sahaskamble <sahaskamble282@gmail.com>
2024-05-10 00:03:44 +0530

    Test commit by Sahas

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+export default function Navbar() {
+	return (
+		<nav>
+
+			<div>
+				Appniche
+			</div>
+
+			<div id="menu_btn">
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+
+			<div id="menu">
+				<span>Project's</span>
+				<span>Blog</span>
+				<span>About</span>
+				<span>More...</span>
+			</div>
+
+		</nav>
+	);
+}
